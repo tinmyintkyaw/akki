4fbc5b8d92959f6b99d577243c3a21eaef92ad0d
tinmyintkyaw <tinmyintkyaw@protonmail.com>
2023-02-24 20:57:54 +0630

    fix content not selectable on firefox

--- components/tiptap-wrappers/ParagraphWrapper.tsx
+++ components/tiptap-wrappers/ParagraphWrapper.tsx
@@ -9,15 +9,15 @@ import { MdDragIndicator } from "react-icons/md";
 export default function ParagraphWrapper(props: NodeViewRendererProps) {
   return (
     <NodeViewWrapper>
-      <div className="w-full h-full flex justify-center p-4 border flex-row select-none">
+      <div className="w-full h-full flex justify-center p-4 border flex-row">
         <div
           data-drag-handle
           contentEditable={false}
           className={clsx("flex items-center")}
-          onClick={() => {
-            props.editor.commands.setContent("<h1>Hello!</h1>");
-            // console.log(props.node.textContent);
-          }}
+          // onClick={() => {
+          //   props.editor.commands.setContent("<h1>Hello!</h1>");
+          //   console.log(props.node.textContent);
+          // }}
         >
           <MdDragIndicator className="w-5 h-7 flex-shrink mr-2" />
         </div>
