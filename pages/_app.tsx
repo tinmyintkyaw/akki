ce4d5de8a50c3228db94672f9f0c17e0cd94d844
tinmyintkyaw <tinmyintkyaw@protonmail.com>
2023-03-19 23:09:33 +0630

    fix: unstable query client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,12 +4,14 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import "@/styles/globals.css";
+import { useState } from "react";
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(() => new QueryClient());
+
   return (
     <SessionProvider session={session}>
       <QueryClientProvider client={queryClient}>
