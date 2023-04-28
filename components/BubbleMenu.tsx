e536dd70497d3d89bb009151325b3073b784b49f
Tin Myint Kyaw <57881874+tinmyintkyaw@users.noreply.github.com>
2023-04-28 16:34:27 +0000

    change bubble menu items styling

--- components/BubbleMenu.tsx
+++ components/BubbleMenu.tsx
@@ -1,4 +1,10 @@
 import { BubbleMenu, Editor } from "@tiptap/react";
+import {
+  MdCode,
+  MdFormatItalic,
+  MdFormatBold,
+  MdStrikethroughS,
+} from "react-icons/md";
 
 export default function SelectMenu(props: { editor: Editor }) {
   return (
@@ -6,37 +12,40 @@ export default function SelectMenu(props: { editor: Editor }) {
       {props.editor && (
         <BubbleMenu
           editor={props.editor}
-          className="rounded border bg-white text-sm shadow-lg drop-shadow-lg"
+          className="flex rounded border bg-white text-sm text-gray-700 shadow-lg drop-shadow-lg"
         >
-          {/* Placeholder for block convert feature */}
-          <button
-            // onClick={() => props.editor.chain().focus().toggleBold().run()}
-            className="h-8 px-3 hover:bg-slate-200"
-          >
-            Paragraph
+          {/* TODO: Implement block convert feature */}
+          <button className="flex h-8 items-center justify-center px-2 text-sm hover:bg-slate-200">
+            <p>Paragraph</p>
           </button>
 
           <button
             onClick={() => props.editor.chain().focus().toggleBold().run()}
-            className="h-8 px-3 hover:bg-slate-200"
+            className="flex h-8 w-8 items-center justify-center line-through hover:bg-slate-200"
           >
-            B
+            <MdFormatBold className="h-4 w-4" />
           </button>
 
           <button
             onClick={() => props.editor.chain().focus().toggleItalic().run()}
-            className="h-8 px-3 hover:bg-slate-200"
+            className="flex h-8 w-8 items-center justify-center line-through hover:bg-slate-200"
           >
-            I
+            <MdFormatItalic className="h-4 w-4" />
           </button>
 
           <button
             onClick={() => props.editor.chain().focus().toggleStrike().run()}
-            className="h-8 px-3 hover:bg-slate-200"
+            className="flex h-8 w-8 items-center justify-center line-through hover:bg-slate-200"
           >
-            S
+            <MdStrikethroughS className="h-4 w-4" />
           </button>
-          {/* <button className="h-8 px-3 hover:bg-slate-200">H3</button> */}
+
+          <button
+            onClick={() => props.editor.chain().focus().toggleCode().run()}
+            className="flex h-8 w-8 items-center justify-center line-through hover:bg-slate-200"
+          >
+            <MdCode className="h-4 w-4" />
+          </button>
         </BubbleMenu>
       )}
     </>
