4449963fc6e566ba02e7fd762a701a9721074544
Tin Myint Kyaw <57881874+tinmyintkyaw@users.noreply.github.com>
2023-05-03 16:32:40 +0000

    implement checklist item toggle logic

--- components/tiptap-wrappers/TaskItemWrapper.tsx
+++ components/tiptap-wrappers/TaskItemWrapper.tsx
@@ -4,13 +4,23 @@ import { MdDragIndicator } from "react-icons/md";
 import BlockWrapper from "./BlockWrapper";
 
 export default function TaskItemWrapper(props: NodeViewProps) {
-  const [showhandle, setShowHandle] = useState(false);
+  const [isChecked, setIsChecked] = useState(props.node.attrs.checked);
 
   return (
     <NodeViewWrapper>
       <BlockWrapper>
         <label contentEditable={false} className="mr-2 select-none">
-          <input type={"checkbox"} className="my-1 h-7 w-4" />
+          <input
+            type={"checkbox"}
+            checked={isChecked}
+            onChange={() => {
+              props.updateAttributes({
+                checked: !isChecked,
+              });
+              setIsChecked(!isChecked);
+            }}
+            className="my-1 h-7 w-4"
+          />
         </label>
 
         <NodeViewContent as={"div"} className="w-full" />
