ed76ac47559336ceb266881da93397c89402ddcc
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2023-07-20 11:42:33 +0630

    feat: add UI to restore or permanently delete pages

---
+++ components/sidebar/DeletedPages.tsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import { useQueryClient } from "@tanstack/react-query";
+import { useRouter } from "next/router";
+import { FileText, Trash2, Undo } from "lucide-react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
+import { ScrollArea } from "../ui/scroll-area";
+import {
+  useRecentPagesQuery,
+  useDeletedPagesQuery,
+  usePermanentlyDeletePageMutation,
+  useUndoDeletePageMutation,
+} from "@/hooks/queryHooks";
+
+const DeletedPages: React.FC = () => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [pageIdToChange, setIdPageToChange] = useState<string>("");
+
+  const router = useRouter();
+  const queryClient = useQueryClient();
+  const recentPagesQuery = useRecentPagesQuery();
+  const deletedPagesQuery = useDeletedPagesQuery();
+  const undoDeletePageMutation = useUndoDeletePageMutation(
+    pageIdToChange,
+    queryClient
+  );
+  const permanentlyDeletePageMutation = usePermanentlyDeletePageMutation(
+    pageIdToChange,
+    queryClient
+  );
+
+  return (
+    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+      <DialogTrigger asChild>
+        <Button variant={"ghost"} className="w-full justify-start">
+          <Trash2 className="mr-2 h-4 w-4" />
+          <span>Trash</span>
+        </Button>
+      </DialogTrigger>
+
+      <DialogContent className="flex h-screen w-screen flex-col md:h-[50vh]">
+        <DialogHeader className="w-full">
+          <DialogTitle>Trash</DialogTitle>
+        </DialogHeader>
+
+        <ScrollArea>
+          {deletedPagesQuery.data &&
+            deletedPagesQuery.data.map((page: any) => (
+              <Button
+                variant={"ghost"}
+                size={"default"}
+                key={page.id}
+                onClick={() => {
+                  router.push(`/${page.id}`);
+                  setIsOpen(false);
+                }}
+                className="w-full"
+                asChild
+              >
+                <div>
+                  <FileText className="mr-2 h-4 w-4" />
+                  <span className="flex-grow text-start">{page.pageName}</span>
+
+                  {/* Buttons nested inside another button  */}
+                  <Button
+                    variant={"ghost"}
+                    size={"icon"}
+                    onClick={(e) => {
+                      setIdPageToChange(page.id);
+                      undoDeletePageMutation.mutate();
+                      setIsOpen(false);
+                    }}
+                  >
+                    <Undo className="h-4 w-4" />
+                  </Button>
+
+                  <Button
+                    variant={"ghost"}
+                    size={"icon"}
+                    onClick={async (e) => {
+                      setIdPageToChange(page.id);
+                      permanentlyDeletePageMutation.mutate();
+
+                      await recentPagesQuery.refetch();
+                      if (!recentPagesQuery.data[0]) router.push("/new");
+                      router.push(`/${recentPagesQuery.data[0].id}`);
+
+                      setIsOpen(false);
+                    }}
+                  >
+                    <Trash2 className="h-4 w-4" />
+                  </Button>
+                </div>
+              </Button>
+            ))}
+        </ScrollArea>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default DeletedPages;
--- components/sidebar/PageList.tsx
+++ components/sidebar/PageList.tsx
@@ -1,11 +1,21 @@
-import { useCreatePageMutation, usePageListQuery } from "@/hooks/queryHooks";
-import * as ScrollArea from "@radix-ui/react-scroll-area";
+import { useState } from "react";
 import { useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/router";
-import { useState } from "react";
-import { MdAdd, MdOutlineDescription } from "react-icons/md";
+import clsx from "clsx";
+
+import { MdOutlineDescription } from "react-icons/md";
+import { FileText, Plus } from "lucide-react";
+
+import {
+  useCreatePageMutation,
+  useFavouritePagesQuery,
+  usePageListQuery,
+} from "@/hooks/queryHooks";
 
 import SidebarMenuLink from "./SidebarMenuLink";
+import { Button } from "../ui/button";
+import { ScrollArea } from "../ui/scroll-area";
+import DeletedPages from "./DeletedPages";
 
 const PageItem = (props: { page: any }) => {
   const [isCollapsed, setIsCollapsed] = useState(true);
@@ -45,8 +55,10 @@ const PageItem = (props: { page: any }) => {
 export default function PageList() {
   const [isFavouritesOpen, setIsFavouritesOpen] = useState(true);
 
+  const router = useRouter();
   const queryClient = useQueryClient();
   const pageListQuery = usePageListQuery();
+  const favouritePagesQuery = useFavouritePagesQuery();
   const createPageMutation = useCreatePageMutation(
     "Untitled",
     null,
@@ -54,30 +66,39 @@ export default function PageList() {
   );
 
   return (
-    <ScrollArea.Root type="auto">
-      <ScrollArea.Viewport className="flex h-[calc(100vh-3rem)] pr-2 text-sm">
-        {/* Favourites List - hides automatically when there are no favourited pages*/}
-        {pageListQuery.data &&
-          pageListQuery.data.filter((page: any) => page.isFavourite).length >
-            0 && (
-            <>
-              <div className="flex w-full flex-row">
-                <button
-                  onClick={() => setIsFavouritesOpen((prev) => !prev)}
-                  className="mx-4 my-1 rounded p-1 text-xs font-semibold hover:bg-accent"
-                >
-                  Favourites
-                </button>
-              </div>
+    <ScrollArea type="auto" className="flex h-[calc(100vh-3rem)] text-sm">
+      {/* Favourites List - hides automatically when there are no favourited pages*/}
+      {favouritePagesQuery.data && favouritePagesQuery.data.length > 0 && (
+        <div className="mb-2 px-2">
+          <button
+            onClick={() => setIsFavouritesOpen((prev) => !prev)}
+            className="mx-4 my-1 rounded p-1 text-xs font-semibold hover:bg-accent"
+          >
+            Favourites
+          </button>
 
-              {isFavouritesOpen &&
-                pageListQuery.data
-                  .filter((page: any) => page.isFavourite)
-                  .map((page: any) => <PageItem key={page.id} page={page} />)}
-            </>
-          )}
+          {isFavouritesOpen &&
+            favouritePagesQuery.data.map((page: any) => (
+              <Button
+                variant={"ghost"}
+                size={"default"}
+                key={page.id}
+                onClick={() => router.push(`/${page.id}`)}
+                className={clsx(
+                  page.id === router.query.pageId &&
+                    "my-1 bg-accent text-accent-foreground",
+                  "w-full"
+                )}
+              >
+                <FileText className="mr-2 h-4 w-4" />
+                <span className="flex-grow text-start">{page.pageName}</span>
+              </Button>
+            ))}
+        </div>
+      )}
 
-        {/* Page List */}
+      {/* Page List */}
+      <div className="mb-2 pr-2">
         <div className="flex w-full flex-row">
           <h2 className="mx-4 my-1 rounded p-1 text-xs font-semibold">Pages</h2>
         </div>
@@ -86,20 +107,20 @@ export default function PageList() {
           pageListQuery.data.map((page: any) => {
             return <PageItem key={page.id} page={page} />;
           })}
+      </div>
 
-        {/* Add Page Button */}
-        <button
-          onClick={() => createPageMutation.mutate()}
-          className="ml-2 flex h-8 w-[calc(100%-0.5rem)] items-center gap-2 rounded-sm px-2 text-sm hover:bg-accent"
-        >
-          <MdAdd className="h-4 w-4" />
-          <p className="line-clamp-1">Add Page</p>
-        </button>
-      </ScrollArea.Viewport>
+      {/* Add Page Button */}
+      <Button
+        variant={"ghost"}
+        size={"default"}
+        onClick={() => createPageMutation.mutate()}
+        className="w-full"
+      >
+        <Plus className="mr-2 h-4 w-4" />
+        <span className="flex-grow text-start">Add Page</span>
+      </Button>
 
-      <ScrollArea.Scrollbar orientation="vertical">
-        <ScrollArea.Thumb className="min-w-[0.5rem] bg-primary/40 hover:bg-primary/60" />
-      </ScrollArea.Scrollbar>
-    </ScrollArea.Root>
+      <DeletedPages />
+    </ScrollArea>
   );
 }
