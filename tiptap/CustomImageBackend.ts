9f4f0802c5dad61341c68c4b8754265025b68f16
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2023-07-28 21:31:06 +0630

    feat: auto upload image on paste

--- tiptap/CustomImageBackend.ts
+++ tiptap/CustomImageBackend.ts
@@ -1,13 +1,26 @@
+import { findChildren } from "@tiptap/core";
 import Image from "@tiptap/extension-image";
 import { Plugin } from "@tiptap/pm/state";
 
+const uploadImage = async (formData: FormData) => {
+  const res = await fetch("/api/images/upload", {
+    method: "POST",
+    body: formData,
+  });
+  const json = await res.json();
+
+  if (json.url && typeof json.url === "string") return json.url;
+  return "";
+};
+
 const CustomImageBackend = Image.extend({
   draggable: true,
+
   addProseMirrorPlugins() {
     return [
       new Plugin({
         props: {
-          handleDrop(view, event, slice, moved) {
+          handleDrop(view, event, moved) {
             if (moved) return false;
             if (!event.dataTransfer?.files[0]) return false;
 
@@ -15,13 +28,8 @@ const CustomImageBackend = Image.extend({
             const formData = new FormData();
             formData.append("image", file);
 
-            fetch("/api/images/upload", {
-              method: "POST",
-              body: formData,
-            })
-              .then((res) => res.json())
-              .then((data) => {
-                const imageURI = data.url;
+            uploadImage(formData)
+              .then((url) => {
                 const { schema } = view.state;
                 const coordinates = view.posAtCoords({
                   left: event.clientX,
@@ -30,7 +38,7 @@ const CustomImageBackend = Image.extend({
 
                 if (!coordinates) return false;
 
-                const node = schema.nodes.image.create({ src: imageURI });
+                const node = schema.nodes.image.create({ src: url });
                 const transaction = view.state.tr.insert(coordinates.pos, node);
                 return view.dispatch(transaction);
               })
@@ -38,9 +46,32 @@ const CustomImageBackend = Image.extend({
 
             return true;
           },
-          handlePaste(view, event, slice) {
+
+          handlePaste(view, event) {
             console.log("paste");
-            console.log(event.clipboardData?.files[0]);
+            const items = Array.from(event.clipboardData?.items || []);
+
+            for (const item of items) {
+              if (item.type.indexOf("image") === 0) {
+                const file = item.getAsFile();
+                if (!file) return false;
+
+                const formData = new FormData();
+                formData.append("image", file);
+
+                uploadImage(formData)
+                  .then((url) => {
+                    const { schema } = view.state;
+                    const node = schema.nodes.image.create({ src: url });
+                    const transaction =
+                      view.state.tr.replaceSelectionWith(node);
+                    view.dispatch(transaction);
+                  })
+                  .catch((err) => console.error(err));
+              }
+            }
+
+            return false;
           },
         },
       }),
