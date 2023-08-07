5b3f1b84fd62f7f0f23577628219fa6fe608d07a
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2023-08-07 08:24:47 +0630

    refactor: remove unused components

--- components/sidebar/PageList.tsx
+++ components/sidebar/PageList.tsx
@@ -3,8 +3,7 @@ import { useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/router";
 import clsx from "clsx";
 
-import { MdOutlineDescription } from "react-icons/md";
-import { FileText, Plus } from "lucide-react";
+import { Plus } from "lucide-react";
 
 import {
   useCreatePageMutation,
@@ -12,46 +11,10 @@ import {
   usePagesListQuery,
 } from "@/hooks/pageQueryHooks";
 
-import SidebarMenuLink from "./SidebarMenuLink";
 import { Button } from "../ui/button";
 import { ScrollArea } from "../ui/scroll-area";
 import DeletedPages from "./DeletedPages";
-import PageTree from "@/components/sidebar/PageTree";
-
-const PageItem = (props: { page: any }) => {
-  const [isCollapsed, setIsCollapsed] = useState(true);
-
-  const router = useRouter();
-
-  function toggleCollapsed() {
-    setIsCollapsed(!isCollapsed);
-  }
-
-  return (
-    <div className="ml-2">
-      <SidebarMenuLink
-        pageId={props.page.id}
-        parentPageId={props.page.parentPageId}
-        isFavourite={props.page.isFavourite}
-        text={props.page.pageName}
-        icon={MdOutlineDescription}
-        isOpen={props.page.id === router.query.pageId}
-        isCollapsed={isCollapsed}
-        setIsCollasped={toggleCollapsed}
-      />
-
-      {!isCollapsed &&
-        props.page.childPages &&
-        props.page.childPages.map((page: any) => (
-          <PageItem key={page.id} page={page} />
-        ))}
-
-      {!isCollapsed && props.page.childPages.length <= 0 && (
-        <div className="my-1 ml-8 text-muted-foreground">No Pages</div>
-      )}
-    </div>
-  );
-};
+import PageTree from "@/components/tree-views/PageTree";
 
 export default function PageList() {
   const [isFavouritesOpen, setIsFavouritesOpen] = useState(true);
@@ -60,10 +23,7 @@ export default function PageList() {
   const queryClient = useQueryClient();
   const pageListQuery = usePagesListQuery();
   const favouritePagesQuery = useFavouritePagesQuery();
-  const createPageMutation = useCreatePageMutation(
-    { pageName: "Untitled", parentPageId: null },
-    queryClient
-  );
+  const createPageMutation = useCreatePageMutation(queryClient);
 
   return (
     <ScrollArea type="auto" className="flex h-[calc(100vh-3rem)] text-sm">
@@ -101,12 +61,11 @@ export default function PageList() {
 
       <div className="mb-2 px-2">
         <div className="my-1 rounded p-1 text-xs font-semibold">Pages</div>
-
-        {/* <PageTree currentlyOpenPage={router.query.pageId as string} /> */}
+        <PageTree />
       </div>
 
       {/* Add Page Button */}
-      <Button
+      {/* <Button
         variant={"ghost"}
         size={"default"}
         onClick={() => createPageMutation.mutate()}
@@ -114,7 +73,7 @@ export default function PageList() {
       >
         <Plus className="mr-2 h-4 w-4" />
         <span className="flex-grow text-start">Add Page</span>
-      </Button>
+      </Button> */}
 
       <DeletedPages />
     </ScrollArea>
