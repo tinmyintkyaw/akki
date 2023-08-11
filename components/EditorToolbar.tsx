82981c8ba3e71f2384f9a476f5dacc8b65ee80eb
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2023-08-11 13:14:01 +0630

    add breadcrumbs to editor toolbar

--- components/EditorToolbar.tsx
+++ components/EditorToolbar.tsx
@@ -1,6 +1,10 @@
+import { useRouter } from "next/router";
+import { MoreHorizontal, PanelLeftOpen, Search } from "lucide-react";
+
+import { usePageQuery } from "@/hooks/pageQueryHooks";
+
 import ToolbarDropdown from "@/components/ToolbarDropdown";
 import SearchComboBox from "@/components/SearchComboBox";
-import { MoreHorizontal, PanelLeftOpen, Search } from "lucide-react";
 import { Button } from "./ui/button";
 
 type EditorToolbarProps = {
@@ -9,11 +13,28 @@ type EditorToolbarProps = {
 };
 
 export default function EditorToolbar(props: EditorToolbarProps) {
+  const router = useRouter();
+  const pageQuery = usePageQuery(router.query.pageId as string);
+
   return (
     <div
       id="editor-toolbar"
       className="flex h-12 w-full select-none items-center gap-2 border-b bg-background px-2 text-foreground shadow-lg"
     >
+      {pageQuery.isLoading || (pageQuery.isError && <div>Loading</div>)}
+
+      {pageQuery.data && (
+        <div className="flex flex-row items-center">
+          <Button variant={"link"} size={"default"} className="h-7">
+            <span>{pageQuery.data.collectionName}</span>
+          </Button>
+          <span>/</span>
+          <Button variant={"link"} size={"default"} className="h-7">
+            <span>{pageQuery.data.pageName}</span>
+          </Button>
+        </div>
+      )}
+
       {!props.isSidebarOpen && (
         <Button
           variant={"ghost"}
