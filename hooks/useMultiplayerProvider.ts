3f7d86b15f1aa3dceebcc4303124ba9d069d0023
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2023-08-14 18:31:00 +0630

    refactor websocket provider hook

---
+++ hooks/useMultiplayerProvider.ts
@@ -0,0 +1,57 @@
+import { HocuspocusProvider, WebSocketStatus } from "@hocuspocus/provider";
+import { useEffect, useRef, useState } from "react";
+import * as Y from "yjs";
+
+const useMultiplayerProvider = (pageId: string) => {
+  const provider = useRef<HocuspocusProvider>();
+
+  const [isAuthenticated, setisAuthenticated] = useState(false);
+  const [isReady, setIsReady] = useState(false);
+  const [status, setStatus] = useState<WebSocketStatus>(
+    WebSocketStatus.Disconnected
+  );
+
+  useEffect(() => {
+    provider.current = new HocuspocusProvider({
+      name: pageId,
+      url: `ws://localhost:8080/collaboration`,
+      document: new Y.Doc(),
+      token: async () => {
+        const response = await fetch("/api/collab/token");
+        if (!response.ok) return "";
+        const json: { collabToken: string } = await response.json();
+        return json.collabToken;
+      },
+      onStatus({ status }) {
+        setStatus(status);
+      },
+      onAuthenticated() {
+        setisAuthenticated(true);
+      },
+      onAuthenticationFailed() {
+        setisAuthenticated(false);
+      },
+      // Event fired on initial successful sync of Y.js document
+      // and set a state to let React know when to re-render the editor
+      onSynced() {
+        setIsReady(true);
+      },
+      onDestroy() {
+        setIsReady(false);
+      },
+    });
+
+    return () => {
+      provider.current?.destroy();
+    };
+  }, [pageId]);
+
+  return {
+    providerRef: provider,
+    isAuthenticated,
+    isReady,
+    status,
+  };
+};
+
+export default useMultiplayerProvider;
