4035d1fa6a931a4e87ee4dd505e426873b38e177
tinmyintkyaw <tinmyintkyaw@protonmail.com>
2023-09-05 12:27:49 +0630

    remove ssr from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,62 +1,31 @@
-import type { InferGetServerSidePropsType, GetServerSideProps } from "next";
-import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]";
-import { prisma } from "@/lib/prismadb";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import { useRecentPagesQuery } from "@/hooks/pageQueryHooks";
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const session = await getServerSession(context.req, context.res, authOptions);
+export default function Home() {
+  const { status } = useSession();
+  const router = useRouter();
+  const recentPagesQuery = useRecentPagesQuery();
 
-  if (!session)
-    return {
-      redirect: {
-        destination: "/signin",
-        permanent: false,
-      },
-    };
+  useEffect(() => {
+    if (recentPagesQuery.isLoading) return;
+    if (status === "loading") return;
+    if (status === "unauthenticated") router.push("/signin");
 
-  try {
-    const recentPages = await prisma.page.findMany({
-      where: {
-        userId: session.accountId,
-        isDeleted: false,
-      },
-      orderBy: {
-        accessedAt: "desc",
-      },
-      take: 3,
-      select: {
-        id: true,
-      },
-    });
-
-    if (recentPages.length > 0) {
-      return {
-        redirect: {
-          destination: `/${recentPages[0].id}`,
-          permanent: false,
-        },
-      };
-    } else {
-      return {
-        redirect: {
-          destination: "/new",
-          permanent: false,
-        },
-      };
+    if (!recentPagesQuery.isError) {
+      if (recentPagesQuery.data.length > 0) {
+        router.push(`/${recentPagesQuery.data[0].id}`);
+      }
+      router.push("/new");
     }
-  } catch (err) {
-    return {
-      redirect: {
-        destination: "/signin",
-        permanent: false,
-      },
-    };
-  }
-};
+  }, [
+    recentPagesQuery.data,
+    recentPagesQuery.isError,
+    recentPagesQuery.isLoading,
+    router,
+    status,
+  ]);
 
-export default function Home(): InferGetServerSidePropsType<
-  typeof getServerSideProps
-> {
-  // Marketing Page
-  return <main></main>;
+  if (status === "loading") return <></>;
 }
