66149f47c1a1972859db19f9b7fe23b49de93179
tinmyintkyaw <tinmyintkyaw@protonmail.com>
2023-09-06 22:07:40 +0630

    update types

--- src/server/middlewares/validate-session.ts
+++ src/server/middlewares/validate-session.ts
@@ -1,14 +1,14 @@
-import { RequestHandler } from "express";
-import { auth } from "@/lucia.js";
+import {RequestHandler} from "express";
+import {auth} from "@/lucia.js";
 
 const validateSession: RequestHandler = async (req, res, next) => {
-  const authRequest = auth.handleRequest(req, res);
-  const session = await authRequest.validate();
+    const authRequest = auth.handleRequest(req, res);
+    const session = await authRequest.validate();
 
-  if (!session) return res.status(401).setHeader("Location", "/signin").end();
+    if (!session) return res.status(401).setHeader("Location", "/signin").end();
 
-  res.locals = session;
-  next();
+    res.locals.session = session;
+    next();
 };
 
 export default validateSession;
