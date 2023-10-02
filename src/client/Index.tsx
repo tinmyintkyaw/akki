7aeecefff9a0141dbd8ceca4f0d4cfb67b0268c0
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2023-10-02 21:25:26 +0630

    fix: show error message when recent pages are empty

--- src/client/Index.tsx
+++ src/client/Index.tsx
@@ -5,50 +5,42 @@ import {
   useRecentPagesQuery,
 } from "@/hooks/pageQueryHooks";
 import { useQueryClient } from "@tanstack/react-query";
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 export default function Index() {
-  const navigate = useNavigate();
   const queryClient = useQueryClient();
   const recentPageListQuery = useRecentPagesQuery();
   const createPageMutation = useCreatePageMutation(queryClient);
 
-  useEffect(() => {
-    if (recentPageListQuery.isError || recentPageListQuery.isLoading) return;
-    navigate(`/${recentPageListQuery.data[0].id}`);
-  }, [
-    navigate,
-    recentPageListQuery.data,
-    recentPageListQuery.isError,
-    recentPageListQuery.isLoading,
-  ]);
-
   return (
     <>
-      {!recentPageListQuery.isLoading &&
-        !recentPageListQuery.isError &&
-        recentPageListQuery.data.length === 0 && (
-          <>
-            <Toolbar title="No Pages Found!" />
+      {!recentPageListQuery.isLoading && !recentPageListQuery.isError && (
+        <>
+          {recentPageListQuery.data.length > 0 ? (
+            <Navigate to={`/${recentPageListQuery.data[0].id}`} />
+          ) : (
+            <>
+              <Toolbar title="No Pages Found!" />
 
-            <div className="flex h-full w-full flex-col items-center gap-3 pt-[30%]">
-              <h1 className="mb-4 text-xl font-semibold">No Pages Found!</h1>
+              <div className="flex h-full w-full flex-col items-center gap-3 pt-[30%]">
+                <h1 className="mb-4 text-xl font-semibold">No Pages Found!</h1>
 
-              <Button
-                variant={"outline"}
-                onClick={() =>
-                  createPageMutation.mutate({
-                    pageName: "Untitled",
-                    parentId: null,
-                  })
-                }
-              >
-                Add New Page
-              </Button>
-            </div>
-          </>
-        )}
+                <Button
+                  variant={"outline"}
+                  onClick={() =>
+                    createPageMutation.mutate({
+                      pageName: "Untitled",
+                      parentId: null,
+                    })
+                  }
+                >
+                  Add New Page
+                </Button>
+              </div>
+            </>
+          )}
+        </>
+      )}
     </>
   );
 }
