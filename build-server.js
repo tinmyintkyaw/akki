71e6b011d49e283fac23614efbde8f6c567ab068
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2023-10-05 05:44:54 +0630

    update backend build script

---
+++ build-server.js
@@ -0,0 +1,29 @@
+import * as esbuild from "esbuild";
+import { copy } from "esbuild-plugin-copy";
+
+(async () => {
+  await esbuild.build({
+    entryPoints: ["./src/server/index.ts"],
+    bundle: true,
+    minify: true,
+    platform: "node",
+    tsconfig: "./src/server/tsconfig.json",
+    outfile: "./dist/server/index.js",
+    logLevel: "info",
+    plugins: [
+      copy({
+        resolveFrom: "cwd",
+        assets: [
+          {
+            from: ["./prisma/*"],
+            to: ["./dist/server"],
+          },
+          {
+            from: ["./node_modules/prisma/libquery_engine-*"],
+            to: ["./dist/server"],
+          },
+        ],
+      }),
+    ],
+  });
+})();
