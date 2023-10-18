25dc0434e6df5edbb0e23a29146110195fb708ab
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2023-10-18 08:17:38 +0630

    add fonts to bundle

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/toaster";
 import { SidebarContextProvider } from "@/contexts/SidebarContext";
-import "@/styles/index.css";
 import queryClient from "@/queryClient";
 import router from "@/router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+
+import "@fontsource-variable/inter";
+
+import "@/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
