afdf553f136fc0fec67ce2878e50f29d6a73fef9
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2023-12-09 10:39:28 +0630

    add delete file controller

--- src/server/routes/file-router.ts
+++ src/server/routes/file-router.ts
@@ -1,6 +1,7 @@
 import getFileController from "@/controllers/files/get-file-controller";
 import uploadFileController from "@/controllers/files/upload-file-controller";
 import multerMiddleware from "@/middlewares/multer";
+import deleteFileController from "@/routes/delete-file-controller";
 import express from "express";
 
 const fileRouter = express.Router();
@@ -8,5 +9,7 @@ const fileRouter = express.Router();
 fileRouter.post("/", multerMiddleware.single("image"), uploadFileController);
 
 fileRouter.get("/:fileId", getFileController);
+
+fileRouter.delete("/:fileName", deleteFileController);
 
 export default fileRouter;
