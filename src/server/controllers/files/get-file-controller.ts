6ad480b39a55506a5c86f8eadb0e3335c0255b38
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2023-12-09 10:49:26 +0630

    fix: improve get file controller error handling

--- src/server/controllers/files/get-file-controller.ts
+++ src/server/controllers/files/get-file-controller.ts
@@ -1,3 +1,5 @@
+import prisma from "@/configs/prisma-client-config";
+import { Prisma } from "@prisma/client";
 import { Request, RequestHandler } from "express";
 import asyncHandler from "express-async-handler";
 import fs from "fs";
@@ -7,16 +9,31 @@ const getFileController: RequestHandler = async (
   req: Request<{ fileId: string }>,
   res,
 ) => {
-  const filePath = path.join(
-    process.cwd(),
-    "uploads",
-    res.locals.session.user.userId,
-    req.params.fileId,
-  );
+  try {
+    await prisma.file.findUniqueOrThrow({
+      where: {
+        id: path.parse(req.params.fileId).name,
+        user_id: res.locals.session.user.userId,
+      },
+    });
 
-  fs.statSync(filePath);
+    const filePath = path.join(
+      process.cwd(),
+      "uploads",
+      res.locals.session.user.userId,
+      req.params.fileId,
+    );
 
-  return res.sendFile(filePath);
+    fs.statSync(filePath);
+
+    return res.sendFile(filePath);
+  } catch (error) {
+    if (
+      error instanceof Prisma.PrismaClientKnownRequestError &&
+      error.code === "P2025"
+    )
+      return res.sendStatus(404);
+  }
 };
 
 export default asyncHandler(getFileController);
