690a2cb6df696d595125b8e89dbe1cfc3987b7eb
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2023-12-31 05:54:06 +0630

    migrate frontend code to monorepo

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import React from "react";
-import App from "./App.tsx";
-import "./index.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
+import queryClient from "@/queryClient";
+import router from "@/router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+
+import "@fontsource-variable/inter";
+import "@fontsource-variable/jost";
+
+import "@/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider defaultTheme="system" storageKey="app-ui-theme">
+      <RouterProvider router={router} />
+      <Toaster />
+    </ThemeProvider>
+    <ReactQueryDevtools initialIsOpen={false} position="bottom" />
+  </QueryClientProvider>,
 );
