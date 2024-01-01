8fda9b9c150b848428cd107ca2e4aaf4085539f1
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2024-01-02 00:20:09 +0630

    migrate rate limiters

---
+++ backend/middlewares/rate-limit.ts
@@ -0,0 +1,18 @@
+import rateLimit from "express-slow-down";
+
+const globalRateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  delayAfter: 50,
+  delayMs: (hits) => hits * 100,
+  legacyHeaders: false,
+});
+
+const sessionRateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  delayAfter: 50,
+  delayMs: (hits) => hits * 100,
+  keyGenerator: async (_req, res) => res.locals.session.sessionId,
+  legacyHeaders: false,
+});
+
+export { globalRateLimiter, sessionRateLimiter };
