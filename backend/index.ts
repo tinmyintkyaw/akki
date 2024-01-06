0dcc4782670d8ced61aecfacfa36deb3b81b9a9f
Tin Myint Kyaw <tinmyintkyaw@protonmail.com>
2024-01-06 12:57:21 +0630

    fix: consistent file naming

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,9 @@
+import { parsedProcessEnv } from "@/configs/env-variables.js";
 import { express } from "@/configs/express.js";
+import { logger } from "@/configs/logger.js";
+import { checkDBConnection } from "@/startup/check-db-connection.js";
 import { checkDemoMode } from "@/startup/check-demo-mode.js";
 import { checkMellisearchDB } from "@/startup/check-meilisearch-db.js";
-import { checkDBConnection } from "@/startup/checkDBConnection.js";
 import { commitMeilisearchCredentialsToRedis } from "@/startup/commit-meilisearch-key-to-redis.js";
 import { migrateToLatest } from "@/startup/migrate-to-latest.js";
 
@@ -11,4 +13,5 @@ await checkMellisearchDB();
 await checkDemoMode();
 await commitMeilisearchCredentialsToRedis();
 
-express.listen(3300, () => console.log("Listening on port 3300"));
+const port = parsedProcessEnv.PORT;
+express.listen(port, () => logger.info(`Listening on port ${port}`));
