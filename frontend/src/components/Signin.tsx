8c154628bc6fd5df626454f4cf287022f906ce55
Tin Myint Kyaw <57881874+tinmyintkyaw@users.noreply.github.com>
2024-12-06 01:31:18 +0000

    add features to frontend auth

--- frontend/src/components/Signin.tsx
+++ frontend/src/components/Signin.tsx
@@ -1,17 +1,20 @@
 import { authClient } from "@/authClient";
 import { Button } from "@/components/ui/button";
 import { useEffect } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const githubSignin = async () => {
-  await authClient.signIn.social(
-    {
-      provider: "github",
-      callbackURL: "/",
-    },
-    { onSuccess: () => console.log("success") },
-  );
-  // await authClient.signOut();
+  await authClient.signIn.social({
+    provider: "github",
+    callbackURL: "/",
+  });
+};
+
+const googleSignin = async () => {
+  await authClient.signIn.social({
+    provider: "google",
+    callbackURL: "/",
+  });
 };
 
 function Signin() {
@@ -21,34 +24,18 @@ function Signin() {
     if (!session.isPending && session.data) navigate("/");
   }, [navigate, session.data, session.isPending]);
 
-  useEffect(() => console.log(session), [session]);
-
-  // const { status } = useSession();
-  // useEffect(() => {
-  //   if (status === "authenticated") navigate("/");
-  // }, [navigate, status]);
-
   return (
     <div className="flex h-screen w-screen select-none flex-col items-center justify-center gap-2">
       {!session.data && (
         <>
           <h1 className="mb-4 text-2xl font-semibold">Sign In</h1>
 
-          <Button variant={"outline"} size={"default"} asChild>
-            <Link to={"/api/signin/google"} reloadDocument className="w-64">
-              <span>{`Continue with Google`}</span>
-            </Link>
+          <Button variant={"outline"} size={"default"} onClick={googleSignin}>
+            <span className="w-56">{`Continue with Google`}</span>
           </Button>
 
-          <Button
-            variant={"outline"}
-            size={"default"}
-            // asChild
-            onClick={githubSignin}
-          >
-            {/* <Link to={"/api/signin/github"} reloadDocument className="w-64"> */}
+          <Button variant={"outline"} size={"default"} onClick={githubSignin}>
             <span className="w-56">{`Continue with GitHub`}</span>
-            {/* </Link> */}
           </Button>
         </>
       )}
