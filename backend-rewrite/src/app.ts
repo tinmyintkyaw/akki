fe6eb4e97df378359503a86469d191c131c09594
Wang Yung Chang <57881874+tinmyintkyaw@users.noreply.github.com>
2026-02-19 11:07:01 +0000

    feat: add debug logging

--- backend-rewrite/src/app.ts
+++ backend-rewrite/src/app.ts
@@ -8,7 +8,9 @@ import {
   ZodTypeProvider,
 } from "fastify-type-provider-zod";
 
-const app = Fastify({ logger: true }).withTypeProvider<ZodTypeProvider>();
+const app = Fastify({
+  logger: { enabled: true, transport: { target: "pino-pretty" } },
+}).withTypeProvider<ZodTypeProvider>();
 
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
@@ -16,5 +18,9 @@ app.setSerializerCompiler(serializerCompiler);
 await app.register(config);
 await app.register(db);
 await app.register(auth);
+
+app.addHook("onRegister", async () => {
+  app.log.level = app.config.NODE_ENV !== "production" ? "debug" : "info";
+});
 
 app.listen({ port: app.config.PORT });
