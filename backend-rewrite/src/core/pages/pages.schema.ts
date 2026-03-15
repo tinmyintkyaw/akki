3c85d66c239674d6898dc7dd19cd5cfe168e0765
tim <tim@example.com>
2026-03-15 15:59:05 +0000

    feat: add cursor pagination to page endpoint

--- backend-rewrite/src/core/pages/pages.schema.ts
+++ backend-rewrite/src/core/pages/pages.schema.ts
@@ -5,12 +5,13 @@ export const pagesReqParams = spacesReqParams.extend({ pageId: z.uuid() });
 
 export const pageTagReqParams = pagesReqParams.extend({ tagId: z.uuid() });
 
-export const pageQueryString = z
-  .object({
-    tag: z.uuid(),
-    deleted: z.stringbool(),
-  })
-  .partial();
+export const pageQueryString = z.object({
+  tag: z.uuid().optional(),
+  deleted: z.stringbool().optional(),
+  untagged: z.stringbool().optional(),
+  cursor: z.uuid().optional(),
+  limit: z.coerce.number().min(10).max(100).default(40),
+});
 
 export const pageCreatePayload = z.object({
   name: z.string().min(1),
@@ -20,3 +21,18 @@ export const pageUpdatePayload = pageCreatePayload
   .extend({ deletedAt: z.iso.datetime().nullable() })
   .partial()
   .refine((obj) => Object.keys(obj).length > 0);
+
+export const pageResponse = z.object({
+  id: z.uuid(),
+  name: z.string(),
+  createdBy: z.uuid(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  deletedAt: z.date().nullable(),
+  spaceId: z.uuid(),
+});
+
+export const pagesResponse = z.object({
+  hasMore: z.boolean(),
+  pages: z.array(pageResponse),
+});
--- backend-rewrite/src/core/pages/routes/get-pages.ts
+++ backend-rewrite/src/core/pages/routes/get-pages.ts
@@ -1,9 +1,10 @@
-import { pageQueryString } from "@/core/pages/pages.schema";
+import { pageQueryString, pagesResponse } from "@/core/pages/pages.schema";
 import { pageSelect } from "@/core/pages/queries/page-select";
 import { getAccessibleSpaces } from "@/core/spaces/spaces.queries";
 import { spacesReqParams } from "@/core/spaces/spaces.schemas";
 import { Session } from "better-auth";
 import { FastifyPluginAsyncZod } from "fastify-type-provider-zod";
+import z from "zod";
 
 // TODO: implement cursor pagination
 export const getPagesRoute: FastifyPluginAsyncZod = async (fastify) => {
@@ -13,13 +14,14 @@ export const getPagesRoute: FastifyPluginAsyncZod = async (fastify) => {
       schema: {
         params: spacesReqParams,
         querystring: pageQueryString,
+        response: { 200: pagesResponse, 400: z.undefined() },
       },
     },
     async (req, res) => {
       const { db } = fastify;
       const { userId } = req.getDecorator<Session>("session");
       const { spaceId } = req.params;
-      const { tag: tagId, deleted, untagged } = req.query;
+      const { tag: tagId, deleted, untagged, limit, cursor } = req.query;
 
       const pages = await db.transaction().execute(async (trx) => {
         await getAccessibleSpaces(trx, {
@@ -31,7 +33,12 @@ export const getPagesRoute: FastifyPluginAsyncZod = async (fastify) => {
           .selectFrom("page")
           .where("spaceId", "=", spaceId)
           .select(pageSelect)
-          .orderBy("id", "desc");
+          .orderBy("id", "desc")
+          .limit(limit + 1);
+
+        if (cursor) {
+          query = query.where("id", "<", cursor);
+        }
 
         if (tagId) {
           query = query
@@ -61,7 +68,13 @@ export const getPagesRoute: FastifyPluginAsyncZod = async (fastify) => {
       });
 
       if (!pages) return res.code(400).send();
-      return res.send(pages);
+
+      const response = {
+        hasMore: pages.length > limit ? true : false,
+        pages: pages.length > limit ? pages.slice(0, -1) : pages,
+      };
+
+      return res.send(response);
     },
   );
 };
